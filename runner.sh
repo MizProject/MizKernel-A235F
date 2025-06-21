73a4b8cf8a3d2324b50f8721a50b29a96e7872dd
SUFandom Xynoxx <bluepearl394@gmail.com>
2025-06-21 22:18:47 +0000

    chore: added KernelSU Next, then changed the builder script to match with A127F in interactivity

---
+++ runner.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+export ARCH=arm64
+export KBUILD_BUILD_USER="@Mizumo_prjkt"
+export KBUILD_BUILD_HOST="MizProject (MIZPRJKT)"
+mkdir out
+
+
+build_kernel() {
+    BUILD_CROSS_COMPILE=$(pwd)/toolchain/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+    KERNEL_LLVM_BIN=$(pwd)/toolchain/llvm-arm-toolchain-ship/10.0/bin/clang
+    CLANG_TRIPLE=aarch64-linux-gnu-
+    KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
+
+    make -j64 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE CONFIG_SECTION_MISMATCH_WARN_ONLY=y vendor/a23_eur_open_defconfig
+    make -j64 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE CONFIG_SECTION_MISMATCH_WARN_ONLY=y
+ 
+    cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+}
+
+submodule_summon() {
+    git submodule init
+    git submoudule update
+}
+
+ksu_symlink() {
+     if [ ! -d "KernelSU-Next/kernel" ]; then
+        echo "Empty KernelSU-Next/kernel directory, abort as extreme error"
+        exit 9
+    fi
+    ln -sf $(pwd)/KernelSU-Next/kernel $(pwd)/drivers/kernelsu
+    if [ $? -ne 0 ]; then
+        echo "Failed to create symlink for KernelSU-Next/kernel"
+        exit 1
+    fi
+}
+
+menuconfig_summon() {
+    make menuconfig
+}
+
+case "$1" in
+    "build")
+        # Use ksu_symlink as always since object files are permanently configured
+        # and to avoid future errors relating "missing file or foler"
+        ksu_symlink
+        if [ "$2" == "-submodule_summon" ]; then
+            submodule_summon
+        fi
+            build_kernel
+        ;;
+    "menuconfig")
+        ksu_symlink
+        menuconfig_summon
+        ;;
+    "submodule_summon")
+        submodule_summon
+        ;;
+    *)
+        echo "Usage: $0 {build (-submodule_summon)|menuconfig|submodule_summon}"
+        exit 1
+        ;;
+esac
